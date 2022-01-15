fca30cbc737b5c6385fac2f7cd7ff6e155405dcb
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-15 23:37:59 +0300

    Реализовал компонент Watches

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { nanoid } from 'nanoid';
 import './App.css';
+import CityModel from './models/CityModel';
+import Watches from './components/Watches';
 
 function App() {
+  const watches = new CityModel(nanoid(), 'Moscow', 3);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Watches watches={watches} onRemove={(id) => console.log(id)} />
+    </>
   );
 }
 
