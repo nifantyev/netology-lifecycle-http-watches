0ab93c61ddc093391f4e689d95b98eeda6fccc81
Евгений Нифантьев <nifantyev@gmail.com>
2022-01-16 12:13:19 +0300

    Реализовал компонент AddCityForm

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { nanoid } from 'nanoid';
 import './App.css';
 import CityModel from './models/CityModel';
 import Watches from './components/Watches';
+import AddCityForm from './components/AddCityForm';
 
 function App() {
   const watches = new CityModel(nanoid(), 'Moscow', 3);
 
+  const handleAdd = (city) => {
+    console.log(city);
+  };
+
   return (
-    <>
+    <div className="container">
+      <AddCityForm onAdd={handleAdd} />
       <Watches watches={watches} onRemove={(id) => console.log(id)} />
-    </>
+    </div>
   );
 }
 
